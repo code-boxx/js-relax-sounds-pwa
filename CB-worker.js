9db43453f9ec88a3f10b23b181f8b2caac9d0624
Code Boxx <code-boxx@mail.com>
2022-12-22 13:16:27 +0800

    22 Dec 2022
    Service worker update.

--- CB-worker.js
+++ CB-worker.js
@@ -1,28 +1,29 @@
-// (A) FILES TO CACHE
-const cName = "JSSleep",
-cFiles = [
-  // (A1) ICONS + FONTS
-  "assets/favicon.png",
-  "assets/icon-512.png",
-  "assets/maticon.woff2",
-  // (A2) APP
-  "assets/js-sleep-sounds.css",
-  "assets/js-sleep-sounds.js",
-  "CB-manifest.json",
-  "js-sleep-sounds.html",
-  // (A3) SOUNDS
-  "assets/chimes.mp3",
-  "assets/crickets.mp3",
-  "assets/rain.mp3"
-];
-
-// (B) CREATE/INSTALL CACHE
-self.addEventListener("install", evt => evt.waitUntil(
-  caches.open(cName)
-  .then(cache => cache.addAll(cFiles))
-  .catch(err => console.error(err))
-));
-
+// (A) CREATE/INSTALL CACHE
+self.addEventListener("install", evt => {
+  self.skipWaiting();
+  evt.waitUntil(
+    caches.open("JSSleep")
+    .then(cache => cache.addAll([
+      // (A1) APP
+      "assets/favicon.png",
+      "assets/icon-512.png",
+      "assets/maticon.woff2",
+      "assets/js-sleep-sounds.css",
+      "assets/js-sleep-sounds.js",
+      "CB-manifest.json",
+      "js-sleep-sounds.html",
+      // (A2) SOUNDS
+      "assets/chimes.mp3",
+      "assets/crickets.mp3",
+      "assets/rain.mp3"
+    ]))
+    .catch(err => console.error(err))
+  );
+});
+ 
+// (B) CLAIM CONTROL INSTANTLY
+self.addEventListener("activate", evt => self.clients.claim());
+ 
 // (C) LOAD FROM CACHE FIRST, FALLBACK TO NETWORK IF NOT FOUND
 self.addEventListener("fetch", evt => evt.respondWith(
   caches.match(evt.request).then(res => res || fetch(evt.request))
